cc485f5cecca9d5a68bb2eb553e89540295ce9ca
Avinash <sreethalam.avinash@gmail.com>
2018-04-29 12:09:50 +0530

    Update assign_1.scala

--- Scala/code/assign_1.scala
+++ Scala/code/assign_1.scala
@@ -2,12 +2,20 @@ class SName(id:Int,name:String,result:Boolean){
 private var s_id = id
 private var s_name = name
 private var s_result = result
+  
+def this(id:Int,name:String)=this(id,name,true)
+def this(id:Int,passed:Boolean)=this(id,"avinash",passed)
+def this(name:String,passed:Boolean)=this(1,name,passed)
+def this()=this(1,"avinash",true)
+  
 def getStudentId() = s_id
 def getStudentName() = s_name
 def getStudentResult() = s_result
+  
 def setStudentId_=(id:Int)= s_id=id
 def setStudentName_=(name:String)= s_name=name
 def setStudentResult_=(result:Boolean)= s_result=result
+  
 def statusReport() ={
 var print_result:String = "passed"
 if(s_result == false)
