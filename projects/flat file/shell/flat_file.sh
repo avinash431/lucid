6d3ac0538b4f1a4f511752824c7e0bed6f319a4c
Avinash <sreethalam.avinash@gmail.com>
2019-06-08 10:55:30 +0530

    Update flat_file.sh

--- projects/flat file/shell/flat_file.sh
+++ projects/flat file/shell/flat_file.sh
@@ -4,18 +4,28 @@
 scripts_dir=$(cd `dirname $0` && pwd)
 
 hive_script_dir=$(cd ${scripts_dir} && cd ../hql && pwd)
-log_dir=$(cd ${scripts_dir} && cd ../logs && pwd)
-
+base_dir=$(cd ${scripts_dir} && cd .. && pwd)
+log_dir=${base_dir}/logs
 hive_script_file=$hive_script_dir/flat_file_insert_branch_details.sql
 
-file_input_loc="/home/cloudera/flat_file/input"
+file_input_loc="/home/cloudera/projects/flat_file/input"
 file_name="database.csv"
 database_name="lucid"
 staging_table_name="branch_details_stg"
 
 BEELINE_COMMAND="jdbc:hive2://"
 
+if [[ ! -d $log_dir ]]
+then
+    mkdir $log_dir
+    touch ${log_dir}/bank_flat_file.log
+fi
+
 exec &>>${log_dir}/bank_flat_file.log
+
+
+echo "log dir is " $log_dir
+echo "hql dir is " $hive_script_dir
 
 #get the staging table and rep table location
 fetch_table_loc(){
