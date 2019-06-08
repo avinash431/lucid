beff30fcd21e45815a08b86e07b5fe7e0d44af4d
Avinash <sreethalam.avinash@gmail.com>
2019-06-08 10:39:27 +0530

    Update xml_create_table.sql

--- projects/xml/hql/xml_create_table.sql
+++ projects/xml/hql/xml_create_table.sql
@@ -1,8 +1,9 @@
+create database lucid if not exists;
 use lucid;
 
-create  table if not exists mydb.xml_temp(line string);
+create  table if not exists xml_temp(line string);
 
-create  table if not exists mydb.books_temp(
+create  table if not exists books_temp(
 author string,
 title array<string>,
 genre array<string>,
@@ -13,42 +14,42 @@ description array<string>)
 row format delimited
 fields terminated by '|';
  
-create  table if not exists mydb.books_temp_title(
+create  table if not exists books_temp_title(
 seqno int,
 author string,
 title string)
 row format delimited
 fields terminated by '|';
 	   
-create  table if not exists mydb.books_temp_genre(
+create  table if not exists books_temp_genre(
 seqno int,
 author string,
 genre  string)
 row format delimited
 fields terminated by '|';
 	   
-create table if not exists mydb.books_temp_price(
+create table if not exists books_temp_price(
 seqno int,
 author string,
 price string)
 row format delimited
 fields terminated by '|';
 
-create table if not exists mydb.books_temp_discount(
+create table if not exists books_temp_discount(
 seqno int,
 author string,
 discount string)
 row format delimited
 fields terminated by '|';
 
-create table if not exists mydb.books_temp_publish_date(
+create table if not exists books_temp_publish_date(
 seqno int,
 author string,
 publish_date string)
 row format delimited
 fields terminated by '|';
 
-create table if not exists mydb.books_temp_description(
+create table if not exists books_temp_description(
 seqno int,
 author string,
 description string)
